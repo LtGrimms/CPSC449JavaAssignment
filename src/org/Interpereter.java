a5c34377e2cb78a820bf1b3c8e5a3949c99f1330
mr-fool <mr-fool@users.noreply.github.com>
2015-03-09 20:28:57 -0600

    Update Interpereter.java
    clicked the wrong shit so I reverted it

--- src/org/Interpereter.java
+++ src/org/Interpereter.java
@@ -1,82 +1,136 @@
-/*References
- * https://www.youtube.com/watch?v=agnblS47F18
- * http://stackoverflow.com/questions/194698/how-to-load-a-jar-file-at-runtime
- * Core Java Volume I Fundamentals Eigth Edition page 222-224*/
-import java.util.*;
-import java.lang.reflect.*;
-import java.io.*;
-import java.net.*;
+package org;
 
-public class Information {
-	public static Class cls;
-	public Information (String jar, String methodsClass){
-		try {
-			File file = new File(jar);
-			URL url = file.toURL();
-			URL[] urls = new URL[] {url};
-			ClassLoader cl = new URLClassLoader(urls);
-			this.cls = cl.loadClass(methodsClass);
-		}
-		catch (NoClassDefFoundError e) {
-			e.printStackTrace();
-			System.exit(-6);
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class Interpereter {
+	
+	private static void printSynopsis() {
+		
 	}
-	public static boolean checkForFunction(String methodName) throws Exception{
-		List<Method> ms = Arrays.asList(cls.getMethods());
-		for (Method m : ms) {
-			if (m.getName().equals(methodName)) {
-				System.out.println("The method exists");
-				return true;
+	
+	private static void printHelp() {
+		
+	}
+	
+	private static void printInterpreterHelp() {
+		
+	}
+
+	private static void unRecognizedQualifierError() {
+		
+		System.exit(-1);
+	}
+	
+	private static void toManyArgumentsError() {
+		
+		System.exit(-2);
+	}
+	
+	private static void jarIsNotAJarError() {
+		
+		System.exit(-3);
+	}
+	
+	private static void helpInWrongLocError() {
+		//NotYet Implemented
+		System.exit(-4);
+	}
+	
+	
+	private static int[] checkQualifiers(String arg, int[] qualifiers) {
+		arg = arg.toLowerCase();
+		int[] newQualifiers = qualifiers;
+			if (arg.charAt(1) == '-') {
+				if      (arg.substring(2) == "help") {
+					newQualifiers[0] = 1;
+					return newQualifiers;
+				}
+				else if (arg.substring(2) == "verbose") {
+					newQualifiers[1] = 1;
+					return newQualifiers;
+				}
+				else unRecognizedQualifierError();
 			}
-		}
-		System.err.println("The method does not exists");
-		return false;
-	}//end method
-	public static void printFunctions () throws Exception{
-		Method[] methods = cls.getDeclaredMethods();
-		for (Method m : methods) {
-			String name = m.getName();
-			System.out.println("The method name is " + name);
-		}
-		
+			for (int i = 1; i < arg.length(); i++) {
+				if (!(arg.charAt(i) == 'h' 
+				   || arg.charAt(i) == 'v' 
+				   || arg.charAt(i) == '?')) {
+					unRecognizedQualifierError();
+				} else if (arg.charAt(i) == 'h' || arg.charAt(i) == '?') {
+					newQualifiers[0] = 1;					
+				} else if (arg.charAt(i) == 'v') {
+					newQualifiers[1] = 1;
+				}	
+			} 
+			return newQualifiers;	
 	}
-	public static ArrayList<Integer> properArguments(String methodName) {
-		ArrayList<Integer> properArguments = new ArrayList<Integer>();
-		List<Method> ms = Arrays.asList(cls.getMethods());
-		for (Method m : ms) {
-			if (m.getName().equals(methodName)) {
-				//return type block
-				if (m.getReturnType().toString() == "String") {
-					properArguments.add(001);
-				}
-				else if (m.getReturnType().toString() == "float") {
-					properArguments.add(100);
-				}
-				else if (m.getReturnType().toString() == "int") {
-					properArguments.add(110);
-				}
-				//parameter block
-				Class[] paramTypes = m.getParameterTypes();
-				for (int j = 0; j < paramTypes.length; j++) {
-					if (paramTypes[j].getName() == "String") {
-						properArguments.add(001);
-					}
-					else if (paramTypes[j].getName() == "float") {
-						properArguments.add(100);
-					}
-					else if (paramTypes[j].getName() == "int") {
-						properArguments.add(110);
-					}
-				}//end for 
+	
+	public static void main(String[] args) {
 
-			}//end if 
-			
+		//// Second try
+		// Still need to ask about the error with exit code -4 since  I dont think 
+		// the loop below will catch it though it could with minor mods
+
+		String jar;
+		String methodsClass;
+		int[] qualifiers;
+		for (String arg : args) {
+			if (arg.charAt(0) == '-') {
+				qualifiers = checkQualifiers(arg, qualifiers);
+				// continue;
+			} else {
+				if (jar == null) {
+					if (/* arg is not a jar */) {
+						jarIsNotAJarError(); // How do we tell the diff between exit code -3 and -5?
+					}
+					jar = arg;
+				} else if (jar != null && methodsClass == null) {
+					methodsClass = arg;
+				} else {
+					toManyArgumentsError(); // Should exit with synopsis
+				}
+			}
+		 }
 		
-		}//end for
-		return properArguments;
-	}//end method 
+		
+		
+		
+		
+		// ------ Basic loop that runs program ----------
+		// Assuming that the 'path' variable is correctly checked (is there two path variables?)
+		// then we can build an information class and pass this into the arborist and evaluator.
+		
+		Information info = new Information(jar, methodsClass); // Needs to exit with code -6 if method class cannot be found and exit code -5 if jar cannot be loaded
+		Arborist arborist = new Arborist(info);
+		TreeEvaluator eval = new TreeEvaluator(info);
+		
+		while (true) {
+			Scanner in = new Scanner(System.in);
+			System.out.print("> ");
+			String argument = in.next();
+			
+			if (argument == "f") {
+				info.printFunctions();
+				continue;
+			} 
+			if (argument == "v") {
+				arborist.toggleVerbose();
+				continue;
+			} 
+			if (argument == "?") {
+				printInterpreterHelp();
+				continue;
+			}
+			if (argument == "q") {
+				System.out.print("bye.");
+				System.exit(0);
+			}
+			
+			ParseTree tree = arborist.growTree(argument);
+			String output = eval.evaluate(tree);
+			System.out.println(output);
+		}
+	}
+
 }
