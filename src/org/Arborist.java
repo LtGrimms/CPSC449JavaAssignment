c3c468a7c93842cf2b26183be71559ae1b04639e
jihyun.kim2 <jihyun.kim2@zone39-wb.cs.ucalgary.ca>
2015-03-13 15:01:47 -0600

    added parseError

--- src/org/Arborist.java
+++ src/org/Arborist.java
@@ -1,20 +1,20 @@
 package org;
 
 //package org;
-
+import java.text.ParseException;
 import java.util.ArrayList;
 
 import utils.ParsingUtils;
 
 public class Arborist {
-	
+
 	private Information info;
 	private boolean verbose;
-	
+
 	// De-Bugging Only
 	public Arborist() {
 	}
-	
+
 	/**
 	 * Creates an Arborist class with desired class information
 	 * @param info The method data that this arborist will use
@@ -22,12 +22,24 @@ public class Arborist {
 	public Arborist(Information info) {
 		this.info = info;
 	}
-	
-	public void checkArgument(String arg) throws Exception {
+
+	private void parseError(String arg, String error,ParseException ex){
+		//ask
+		System.out.println(error+" at offest "+ex.getErrorOffset());
+		System.out.println(arg);
+		for(int i=0;i<ex.getErrorOffset();i++){
+			System.out.print("-");
+		}
+		System.out.print("^\n");
+		if(verbose)
+			ex.printStackTrace();
+	}
+
+	public void checkArgument(String arg){
 		checkArgument(arg, new int[] {0, Integer.MAX_VALUE, 0});
 	}
-	
-	public int[] checkArgument(String arg, int[] argumentData) throws Exception {
+
+	public int[] checkArgument(String arg, int[] argumentData){
 		int end;
 		int index = argumentData[0];
 		int type = argumentData[2];
@@ -38,12 +50,15 @@ public class Arborist {
 
 		if (arg.charAt(index) == '(') {										
 			end = ParsingUtils.findClosingBracket(arg, index);				
-			if (end > argumentData[1]) 
-				throw new Exception();
-
+			if (end > argumentData[1]) {
+				ParseException ex=new ParseException("could not find closing bracket",index);
+				parseError(arg,"could not find closing bracket",ex);
+			}
 			String function = ParsingUtils.nextWord(arg, index + 1);		
-			//checkForFunction(function);
-
+			if(!info.checkForFunction(function)){
+				ParseException ex=new ParseException("could not find function.",index);
+				parseError(arg,"could not find function.",ex);
+			}
 			int nextIndex = index + function.length() + 1;									
 			ArrayList<Integer> argTypes = new ArrayList<Integer>();
 			while (nextIndex < end) {
@@ -52,8 +67,11 @@ public class Arborist {
 				argTypes.add(argData[2]);
 			}
 			type = ParsingUtils.checkForProperArguments(function, argTypes);
-			if (type == 0)
-				throw new Exception("Something had the wrong type");
+			if (type == 0){
+				ParseException ex=new ParseException("Something had the wrong type",index);
+				parseError(arg,"Something had the wrong type",ex);
+			}
+
 			return new int[] {index,end,type};
 
 		} else if (arg.charAt(index) == '"') {
@@ -74,8 +92,8 @@ public class Arborist {
 		ArrayList<String> splitArguments = new ArrayList<String>();
 		for (int i = 0; i < arg.length(); i++) {
 			if (arg.charAt(i) == '(' 
-			 || arg.charAt(i) == ')'
-			 || arg.charAt(i) == ' ') {
+					|| arg.charAt(i) == ')'
+					|| arg.charAt(i) == ' ') {
 				continue;
 			} else if (arg.charAt(i) == '\"') {
 				int closingIndex = ParsingUtils.findClosingQuote(arg, i) + 1;
@@ -87,24 +105,24 @@ public class Arborist {
 				i += word.length();
 			}
 		}
-		
+
 		for (String spurt : splitArguments) {
 			tree.grow(spurt);
 		}
 		tree.addReturnTypes();
-		
+
 		return tree;
 	}
-	
+
 	public void toggleVerbose() {
 		verbose = !verbose;
 	}
 
 	public static void main(String[] args) throws Exception {
-		
+
 		Arborist amy=new Arborist();
 		String arg = "(add (mult 5 5) (mult 5 5))";
-		
+
 		amy.checkArgument(arg, new int[] {0, Integer.MAX_VALUE, 0});
 		ParseTree tree=amy.growTree(arg);
 		System.out.println(tree.toString());
