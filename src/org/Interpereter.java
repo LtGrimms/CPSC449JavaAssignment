b6bbc270bb35e3d93cb080be1af6e833782d5ce7
LtGrimms <alcridla@gmail.com>
2015-03-14 11:29:45 -0600

    comments and some additional functionallity

--- src/org/Interpereter.java
+++ src/org/Interpereter.java
@@ -3,97 +3,126 @@ package org;
 import java.nio.file.Path;
 import java.util.Scanner;
 
+import utils.ErrorUtils;
+
 public class Interpereter {
 	
+	/**
+	 * Pretty self explanatory, this prints the synopsis for the program. This is
+	 * run before the program enters interpreter mode.
+	 */
 	private static void printSynopsis() {
-		
+		System.out.println("Synopsis:");
+		System.out.println("  methods");
+		System.out.println("  methods { -h | -? | --help }+");
+		System.out.println("  methods {-v --verbose}* <jar-file> [<class-name>]");
+		System.out.println("Arguments:");
+		System.out.println("  <jar-file>:   The .jar file that contains the class to laod (see next line).");
+		System.out.println("  <class-name>: The full qualified class name containing publc static command methods to call.");
+		System.out.println("Qualifiers:");
+		System.out.println("  -v --verbose: Print out detailed errors, warning, and tracking.");
+		System.out.println("  -h -? --help: Pring out a detailed help message.");
+		System.out.println("Single-char qualifiers may be grouped; long qualifiers may be truncated to unique prefixes and are not case sensitive.");
 	}
 	
+	/**
+	 * adds an extra line to the print synopsis function describing proper input
+	 * for the program.
+	 */
 	private static void printHelp() {
-		
+		printSynopsis();
+		System.out.println("This program interprets commands of the format '(<method> {arg}*)' on the command lind, finds cooresponding methods in <class-name>, and executes them, printing the result to sysout.");
 	}
 	
+	/**
+	 * This function is called inside the interpreter and prints out the interpreters 
+	 * help text.
+	 */
 	private static void printInterpreterHelp() {
-		
-	}
-
-	private static void unRecognizedQualifierError() {
-		
-		System.exit(-1);
-	}
-	
-	private static void toManyArgumentsError() {
-		
-		System.exit(-2);
+		System.out.println("q            : Quit the program.");
+		System.out.println("v            : Toggle verbose mode (stack traces).");
+		System.out.println("f            : List all know functions.");
+		System.out.println("?            : Print this helpful text.");
+		System.out.println("<expression> : Evaluate the expression.");
+		System.out.println("Expressions can be integers, floats, strings (sourrounded in double quotes) or function calls of the form '(identifier {expression}*)'.");
 	}
 	
-	private static void jarIsNotAJarError() {
-		
-		System.exit(-3);
-	}
-	
-	private static void helpInWrongLocError() {
-		//NotYet Implemented
-		System.exit(-4);
-	}
-	
-	
-	private static int[] checkQualifiers(String arg, int[] qualifiers) {
+	/**
+	 * This is to check the input arguments for the help or verbose qualifers.
+	 * The method assumes that the string it is being passed has a '-' as the
+	 * first character
+	 * @param arg the string to be checked for qualifiers
+	 * @param qualifiers a boolean array representing whether or not help and verbose have been found
+	 * @return
+	 */
+	private static boolean[] checkQualifiers(String arg, boolean[] qualifiers) {
 		arg = arg.toLowerCase();
-		int[] newQualifiers = qualifiers;
+		boolean[] newQualifiers = qualifiers;
 			if (arg.charAt(1) == '-') {
 				if      (arg.substring(2) == "help") {
-					newQualifiers[0] = 1;
+					newQualifiers[0] = true;
 					return newQualifiers;
 				}
 				else if (arg.substring(2) == "verbose") {
-					newQualifiers[1] = 1;
+					newQualifiers[1] = true;
 					return newQualifiers;
 				}
-				else unRecognizedQualifierError();
+				else ErrorUtils.unRecognizedQualifierError();
 			}
 			for (int i = 1; i < arg.length(); i++) {
 				if (!(arg.charAt(i) == 'h' 
 				   || arg.charAt(i) == 'v' 
 				   || arg.charAt(i) == '?')) {
-					unRecognizedQualifierError();
+					ErrorUtils.unRecognizedQualifierError();
 				} else if (arg.charAt(i) == 'h' || arg.charAt(i) == '?') {
-					newQualifiers[0] = 1;					
+					newQualifiers[0] = true;					
 				} else if (arg.charAt(i) == 'v') {
-					newQualifiers[1] = 1;
+					newQualifiers[1] = true;
 				}	
 			} 
-			return newQualifiers;	
+			return newQualifiers;
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
-		//// Second try
 		// Still need to ask about the error with exit code -4 since  I don't think 
 		// the loop below will catch it though it could with minor mods
 
-		String jar;
-		String methodsClass;
-		int[] qualifiers;
+		String jar = null;
+		String methodsClass = null;
+		boolean[] qualifiers = {false, false};
 		for (String arg : args) {
 			if (arg.charAt(0) == '-') {
 				qualifiers = checkQualifiers(arg, qualifiers);
 				// continue;
 			} else {
 				if (jar == null) {
-					if (/* arg is not a jar */) {
-						jarIsNotAJarError(); // How do we tell the diff between exit code -3 and -5?
+					if (/* arg is not a jar */false) {
+						ErrorUtils.jarIsNotAJarError(); // How do we tell the diff between exit code -3 and -5?
 					}
 					jar = arg;
 				} else if (jar != null && methodsClass == null) {
 					methodsClass = arg;
 				} else {
-					toManyArgumentsError(); // Should exit with synopsis
+					ErrorUtils.toManyArgumentsError(); // Should exit with synopsis
 				}
 			}
 		 }
 		
+		boolean help = qualifiers[0];
+		boolean verb = qualifiers[1];
 		
+		if (jar == null && !help) {
+			printSynopsis();
+			System.exit(0); //Double check this
+		} else if (help) {
+			printHelp();
+			if (jar == null)
+				System.exit(0);
+		}
+		
+		if (jar != null && methodsClass == null)
+			methodsClass = "Commands";
 		
 		
 		
@@ -102,35 +131,40 @@ public class Interpereter {
 		// then we can build an information class and pass this into the arborist and evaluator.
 		
 		Information info = new Information(jar, methodsClass); // Needs to exit with code -6 if method class cannot be found and exit code -5 if jar cannot be loaded
-		Arborist arborist = new Arborist(info);
-		TreeEvaluator eval = new TreeEvaluator(info.getClass());
+		Arborist arborist = new Arborist(info, verb);
+		//TreeEvaluator eval = new TreeEvaluator(info.getClass());
 		
 		while (true) {
 			Scanner in = new Scanner(System.in);
 			System.out.print("> ");
-			String argument = in.next();
+			String argument = in.nextLine();
 			
-			if (argument == "f") {
+			boolean lengthOne = argument.length() == 1;
+			char firstChar = argument.charAt(0);
+			
+			if (lengthOne && firstChar == 'f') {
 				info.printFunctions();
 				continue;
 			} 
-			if (argument == "v") {
+			if (lengthOne && firstChar == 'v') {
 				arborist.toggleVerbose();
 				// If anything else needs to toggle verbose put it here
 				continue;
 			} 
-			if (argument == "?") {
+			if (lengthOne && firstChar == '?') {
 				printInterpreterHelp();
 				continue;
 			}
-			if (argument == "q") {
+			if (lengthOne && firstChar == 'q') {
 				System.out.print("bye.");
 				System.exit(0);
 			}
 			
 			ParseTree tree = arborist.growTree(argument);
-			String output = eval.evaluate(tree);
+			String output = tree.toString(); //eval.evaluateTree(tree);
 			System.out.println(output);
+			
+			//in.close(); //not sure why but this causes an error
 		}
 	}
 
