3e4de726b5c7c214fd8851e6d669bd43f4908899
LtGrimms <alcridla@gmail.com>
2015-03-17 01:14:19 -0600

    the arborist will now grow a tree as it checks arguments

--- src/org/Arborist.java
+++ src/org/Arborist.java
@@ -8,6 +8,7 @@ import utils.ParsingUtils;
 
 public class Arborist {
 
+	private ParseTree tree;
 	private Information info;
 	private boolean verbose;
 
@@ -25,13 +26,16 @@ public class Arborist {
 	}
 
 	/**
-	 * Before an Arborist grows a tree it will check the string for errors. This
-	 * is the function that checks strings for errors
-	 * @param arg The string to check for errors
-	 * @throws Exception 
+	 * This function simultaneously checks a string for errors and grows an appropriate parse tree.
+	 * The function will complete without error iff the input string was valid with respect to the
+	 * reflection data. (double check that)
+	 * @param arg The string to check for errors and attempt to grow
+	 * @throws Exception parse exceptions are thrown if the string is invalid
 	 */
-	public void checkArgument(String arg) throws Exception{
+	public ParseTree checkArgument(String arg) throws Exception{
+		tree = new ParseTree(info);
 		checkArgument(arg, new int[] {0, Integer.MAX_VALUE - 1, 0});
+		return tree;
 	}
 
 	/**
@@ -41,8 +45,8 @@ public class Arborist {
 	 * the function has the correct input parameters.
 	 * @param arg The string to be error checked
 	 * @param argumentData an array in the form {start-checking-here, end-checking-here, function-return-type}
-	 * @return This will either print an error message or its return value is only usefull for recusive calls
-	 * @throws Exception 
+	 * @return This will either print an error message or do nothing. Its return value is only usefull for recusive calls
+	 * @throws Exception invalid strings will throw parse errors
 	 */
 	public int[] checkArgument(String arg, int[] argumentData) throws Exception{
 		int end;
@@ -53,78 +57,73 @@ public class Arborist {
 			return checkArgument(arg, new int[] {index+1, argumentData[1], type});
 		}
 
-		if (arg.charAt(index) == '(') {										
+		if (arg.charAt(index) == '(') {	
 			end = ParsingUtils.findClosingBracket(arg, index);				
 			if (end > argumentData[1]) {
 				ParseException ex=new ParseException("could not find closing bracket",index);
 				ErrorUtils.parseError(arg,"could not find closing bracket",ex,verbose);
 			}
-			String function = ParsingUtils.nextWord(arg, index + 1);		
-			if(!info.checkForFunction(function)){
-				ParseException ex=new ParseException("could not find function.",index);
-				ErrorUtils.parseError(arg,"could not find function.",ex,verbose);
-			}
-			int nextIndex = index + function.length() + 1;									
-			ArrayList<Integer> argTypes = new ArrayList<Integer>();
-			while (nextIndex < end) {
-				int[] argData = checkArgument(arg, new int[] {nextIndex, end, 0});
-				nextIndex = argData[1] + 1;									
-				argTypes.add(argData[2]);
-			}
-			type = ParsingUtils.checkForProperArguments(function, argTypes);
-			if (type == 0){
-				ParseException ex=new ParseException("Something had the wrong type",index);
-				ErrorUtils.parseError(arg,"Something had the wrong type",ex,verbose);
-			}
-
-			return new int[] {index,end,type};
+			
+			return checkFunction(arg, new int[] {index, end, type});
 
 		} else if (arg.charAt(index) == '"') {
 			end = ParsingUtils.findClosingQuote(arg, index);
+			if (end > argumentData[1]) {
+				ParseException ex=new ParseException("could not find closing quote",index);
+				ErrorUtils.parseError(arg,"could not find closing quote",ex,verbose);
+			}
+			tree.grow(arg.substring(index, end + 1), 0);
 			return new int[] {index, end, 1};
 
 		} else {
-			end = ParsingUtils.findNextSpace(arg, index);
-			int end2 = ParsingUtils.findClosingBracket(arg, index);
-			if (end2 < end) end = end2;
+			end = ParsingUtils.findEndOfArgument(arg, index);
 			type = ParsingUtils.intOrFloat(arg, index);
+			if (type == 0) {
+				ParseException ex = new ParseException("Could not identifiy argument", index);
+				ErrorUtils.parseError(arg, "Could not identify argument" ,ex, verbose);
+			}
+
+			tree.grow(arg.substring(index, end), 0);
+			
 			return new int[] {index, end, type};
 		}
 	}
-
+	
 	/**
-	 * This function will check a string for errors with checkArgument either grow
-	 * the corresponding tree or throw an exception.
-	 * @param arg The string that grows the tree
-	 * @return returns a parse tree corresponding to the string
-	 * @throws Exception
+	 * if checkArgument encounters a function it will use this to recursively call itself on the functions
+	 * arguments.
+	 * @param arg the string that is being checked for errors
+	 * @param argumentData same as in checkArgument
+	 * @return same as in checkArgument, only usefull for recursive calls
+	 * @throws Exception parse exceptions are throw for invalid strings.
 	 */
-	public ParseTree growTree(String arg) throws Exception{
-		checkArgument(arg);
-		ParseTree tree = new ParseTree();
-		ArrayList<String> splitArguments = new ArrayList<String>();
-		for (int i = 0; i < arg.length(); i++) {
-			if (arg.charAt(i) == '(' 
-					|| arg.charAt(i) == ')'
-					|| arg.charAt(i) == ' ') {
-				continue;
-			} else if (arg.charAt(i) == '\"') {
-				int closingIndex = ParsingUtils.findClosingQuote(arg, i) + 1;
-				splitArguments.add(arg.substring(i, closingIndex));
-				i = closingIndex;
-			} else {
-				String word = ParsingUtils.nextWord(arg, i);
-				splitArguments.add(word);
-				i += word.length();
-			}
+	private int[] checkFunction(String arg, int[] argumentData) throws Exception {
+		int index = argumentData[0];
+		int end = argumentData[1];
+		int type = argumentData[2];
+		
+		String function = ParsingUtils.nextWord(arg, index + 1);		
+		if(!info.checkForFunction(function)){
+			ParseException ex=new ParseException("could not find function " + function, index);
+			ErrorUtils.parseError(arg,"could not find function " + function ,ex,verbose);
+		}
+		int numberOfArguments = 2; //ParsingUtils.numberOfArguments(arg, index, end);
+		tree.grow(function, numberOfArguments);
+		
+		int nextIndex = index + function.length() + 1;									
+		ArrayList<Integer> argTypes = new ArrayList<Integer>();
+		while (nextIndex < end) {
+			int[] argData = checkArgument(arg, new int[] {nextIndex, end, 0});
+			nextIndex = argData[1] + 1;									
+			argTypes.add(argData[2]);
+		}
+		type = ParsingUtils.checkForProperArguments(function, argTypes);
+		if (type == 0){
+			ParseException ex=new ParseException("Something had the wrong type",index);
+			ErrorUtils.parseError(arg,"Something had the wrong type",ex,verbose);
 		}
 
-		for (String spurt : splitArguments) {
-			tree.grow(spurt);
-		}
-		tree.addReturnTypes();
-
-		return tree;
+		return new int[] {index,end,type};
 	}
 
 	/**
@@ -139,8 +138,10 @@ public class Arborist {
 		Arborist amy=new Arborist(new Information("/Users/alcridla/Documents/Methods.jar", "tests.Methods01"), false);
 		String arg = "\"hello\"";
 
-		amy.growTree(arg);
-		ParseTree tree=amy.growTree(arg);
-		System.out.println(tree.toString());
+		System.out.println(1100 & 1000);
+		ParseTree tree;
+		tree = amy.checkArgument("Hello\"");
+		tree.addReturnTypes();
+		System.out.println(tree);
 	}
 }
