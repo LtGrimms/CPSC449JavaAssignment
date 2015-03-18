e7cbe807d0d1fab74efff4e42e0f564e24a46a6d
LtGrimms <alcridla@gmail.com>
2015-03-18 17:50:27 -0600

    better

--- src/org/Information.java
+++ src/org/Information.java
@@ -10,6 +10,9 @@ import java.io.*;
 import java.net.*;
 
 public class Information {
+	public static final int BIGINTEGER = 14, INTEGER = 6, BIGFLOAT = 12, FLOAT = 4, STRING = 1;
+	
+	
 	public static Class cls;
 	public Information (String jar, String methodsClass){
 		try {
@@ -74,88 +77,93 @@ public class Information {
 		}
 		
 	}
-	public static List<List<Integer>> properArguments(String methodName) {
-		List<List<Integer>> properArguments = new ArrayList<List<Integer>>(); 
+	public static ArrayList<Integer[]> properArguments(String methodName) {
+		ArrayList<Integer[]> properArguments = new ArrayList<Integer[]>(); 
 		List<Method> ms = Arrays.asList(cls.getMethods());
-		List<Integer> myList = new ArrayList<Integer>();
 		for (Method m : ms) {
 			if (m.getName().equals(methodName)) {
 				//return type block
 				//System.out.println("The method exists");
+				Integer[] instanceOfProperArguments = new Integer[m.getParameterTypes().length + 1];
 				String returnType = m.getReturnType().toString();
 				if (returnType.contains(".")) {
 					int start = returnType.lastIndexOf(".") + 1;
 					int end = returnType.length();
-					String formatted = returnType.substring(start,end).toLowerCase();
+					String formatted = returnType.substring(start,end);
 					//System.out.println("The formatted " + formatted);
-					if (formatted.equals("string")) {
+					if (formatted.equals("String")) {
 						//System.out.println("formatted reach");
-						myList.add(1);
+						instanceOfProperArguments[0] = STRING;
+					}
+					else if (formatted.equals("Float")) {
+						instanceOfProperArguments[0] = BIGFLOAT;
 					}
 					else if (formatted.equals("float")) {
-						myList.add(4);
+						instanceOfProperArguments[0] = FLOAT;
+					}
+					else if (formatted.equals("Integer") ) {
+						instanceOfProperArguments[0] = BIGINTEGER;
 					}
 					else if (formatted.equals("int") ) {
-						myList.add(6);
-					}
-					else if (formatted.equals("integer") ) {
-						myList.add(6);
+						instanceOfProperArguments[0] = INTEGER;
 					}
 				}
 				//will never be reach but just in case
-				else if (m.getReturnType().toString().equals("String")) {
-					System.out.println("formatted reach");
-					myList.add(1);
-				}
-				else if (m.getReturnType().toString().equals("float") ){
-					myList.add(4);
-				}
-				else if (m.getReturnType().toString().equals("int") ){
-					myList.add(6);
-				}
-				else if (m.getReturnType().toString().equals("Integer") ){
-					myList.add(6);
-				} 
+//				else if (m.getReturnType().toString().equals("String")) {
+//					System.out.println("formatted reach");
+//					instanceOfProperArguments[0] = 1;
+//				}
+//				else if (m.getReturnType().toString().equals("float") ){
+//					instanceOfProperArguments[0] = 4;
+//				}
+//				else if (m.getReturnType().toString().equals("int") ){
+//					instanceOfProperArguments[0] = 6;
+//				}
+//				else if (m.getReturnType().toString().equals("Integer") ){
+//					instanceOfProperArguments[0] = 6;
+//				} 
 				//parameter block
 				Class[] paramTypes = m.getParameterTypes();
-				for (int j = 0; j < paramTypes.length; j++) {
-					String parameter = paramTypes[j].getName();
+				for (int j = 1; j < instanceOfProperArguments.length; j++) {
+					String parameter = paramTypes[j - 1].getName();
 					if (parameter.contains(".") ){
 						int start = parameter.lastIndexOf(".") + 1;
 						int end = parameter.length();
 						String formatted = parameter.substring(start,end).toLowerCase();
-						if (formatted.equals("string")) {
-							myList.add(1);
+						if (formatted.equals("String")) {
+							instanceOfProperArguments[j] = STRING;
+						}
+						else if (formatted.equals("Float")) {
+							instanceOfProperArguments[j] = BIGFLOAT;
 						}
 						else if (formatted.equals("float")) {
-							myList.add(4);
+							instanceOfProperArguments[j] = FLOAT;
+						}
+						else if (formatted.equals("Integer")) {
+							instanceOfProperArguments[j] = BIGINTEGER;
 						}
 						else if (formatted.equals("int") ) {
-							myList.add(6);
-						}
-						else if (formatted.equals("integer") ) {
-							myList.add(6);
+							instanceOfProperArguments[j] = INTEGER;
 						}
 						}//end parameter contains
 					//never gonna reach it unless shit happens
-					else if (paramTypes[j].getName().equals("String")) {
-						myList.add(1);
-					}
-					else if (paramTypes[j].getName().equals("float") ){
-						myList.add(4);
-					}
-					else if (paramTypes[j].getName().equals("int") ){
-						myList.add(6);
-					}
-					else if (paramTypes[j].getName().equals("Integer") ){
-						myList.add(6);
-					}
+//					else if (paramTypes[j].getName().equals("String")) {
+//						instanceOfProperArguments[j] = 1;
+//					}
+//					else if (paramTypes[j].getName().equals("float") ){
+//						instanceOfProperArguments[j] = 4;
+//					}
+//					else if (paramTypes[j].getName().equals("int") ){
+//						instanceOfProperArguments[j] = 6;
+//					}
+//					else if (paramTypes[j].getName().equals("Integer") ){
+//						instanceOfProperArguments[j] = 6;
+//					}
 					
 				}//end for 
-
+				properArguments.add(instanceOfProperArguments);
 			}//end if 
 			
-		properArguments.add(myList);
 		}//end for
 		return properArguments;
 	}//end method 
